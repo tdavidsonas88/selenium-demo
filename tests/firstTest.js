fedfd01a1842ee68c7d08439a2b9d3fe11848bb5
Tadas Davidsonas <tdavidsonas88@gmail.com>
2023-02-03 11:55:04 +0200

    assert lib assertion example

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,4 +1,5 @@
 const {Builder, By, Key} = require("selenium-webdriver");
+const assert = require("assert");
 
 async function example() {
 
@@ -11,6 +12,13 @@ async function example() {
     // add a todo
     await driver.findElement(By.id("sampletodotext"))
         .sendKeys("Learn Selenium", Key.RETURN);
+
+    // assert
+    let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+        return value;
+    })
+
+    assert.strictEqual(todoText, "Learn Selenium");
 
     // close the browser
     await driver.quit();
