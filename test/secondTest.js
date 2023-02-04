3ce4c69974252642c00a809dc1f7a55f86da91a9
tdavidsonas88 <tdavidsonas88@gmail.com>
2023-02-04 16:01:55 +0200

    running tests in parallel with mocha

---
+++ test/secondTest.js
@@ -0,0 +1,31 @@
+const {Builder, By, Key} = require("selenium-webdriver");
+var should = require("chai").should();
+
+// describe block
+describe("add another todo tests", function(){
+
+    // it block
+    it("successfully add a another todo to application", async function(){
+        // launch the browser
+        let driver = await new Builder().forBrowser("firefox").build();
+
+        // navigate to application
+        await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+        // add a todo
+        await driver.findElement(By.id("sampletodotext"))
+            .sendKeys("Learn Selenium", Key.RETURN);
+
+        // assert
+        let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+            return value;
+        })
+
+        // assert using chai should
+        todoText.should.equal("Learn Selenium");
+
+        // close the browser
+        await driver.quit();            
+    });
+
+});
