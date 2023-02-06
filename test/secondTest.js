9ba18cc78ad5621deee73199075fcb3a6168e731
Tadas Davidsonas <tdavidsonas88@gmail.com>
2023-02-06 13:37:34 +0200

    reports with mochawesome

--- test/secondTest.js
+++ test/secondTest.js
@@ -28,4 +28,27 @@ describe("add another todo tests", function(){
         await driver.quit();            
     });
 
+    it("Adding a new test for reporting", async function(){
+        // launch the browser
+        let driver = await new Builder().forBrowser("firefox").build();
+
+        // navigate to application
+        await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+        // add a todo
+        await driver.findElement(By.id("sampletodotext"))
+            .sendKeys("Learn Selenium", Key.RETURN);
+
+        // assert
+        let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+            return value;
+        })
+
+        // assert using chai should
+        todoText.should.equal("Learn JavaScript");
+
+        // close the browser
+        await driver.quit();            
+    });
+
 });
